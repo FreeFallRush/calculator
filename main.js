3c1d7c5426a114f989f3dc8cd51676180f05dd25
FreeFallRush <queenofcups.altbot@gmail.com>
2023-01-10 23:15:41 +0200

    Updated main.js
    Added event listeners for numbers and operators btn

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ const clearAll = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
 const currentDisplay = document.getElementById("current");
 currentDisplay.textContent = displayValue;
+
+//add event listeners
+numbers.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    manageNumbers(e.target.textContent);
+  });
+});
+
+operators.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    manageOperators(e.target.textContent);
+  });
+});
